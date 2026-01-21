0d17eaa1c6a843dc91ac13a61a054f43d7dba5c3
Joel B Joseph <your-email@example.com>
2026-01-21 16:56:23 +0530

    video-animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {useGSAP} from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+    const containerRef = useRef();
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title',{type: 'chars, words'});
@@ -37,10 +44,33 @@ const Hero = () => {
         })
             .to('.right-leaf', {y: 200}, 0)
             .to('.left-leaf', {y: 200}, 0)
-    })
+            .to(videoRef.current, {
+                currentTime: videoRef.current.duration || 5,
+                ease: 'none',
+            }, 0)
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : "bottom top";
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+    }, []);
 
     return (
-        <>
+        <div ref={containerRef}>
             <section id={"hero"} className={"noisy"}>
                 <h1 className={"title"}>MOJITO</h1>
 
@@ -64,7 +94,11 @@ const Hero = () => {
                 </div>
 
             </section>
-        </>
+
+            <div className={"video-container absolute inset-0"}>
+                <video ref={videoRef} src={"/videos/input.mp4"} muted playsInline preload="auto" className={"video"}/>
+            </div>
+        </div>
     );
 };
 
